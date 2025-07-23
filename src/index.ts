ebab9a51669c04e70653835340cccee20dca3300
Sebola3461 <paulo.8655142@aluno.mg.gov.br>
2025-07-22 21:07:47 -0300

    Refactor main file structure

--- src/index.ts
+++ src/index.ts
@@ -1,47 +1,62 @@
-import { Client, GatewayIntentBits, RESTPostAPIApplicationCommandsJSONBody } from "discord.js";
-import chalk from "chalk";
-import { registerCommands } from "./utils/registerCommand";
+import { Client, GatewayIntentBits } from "discord.js";
 import { config } from "dotenv";
+import { Logger } from "./utils/Logger";
 
 config();
-const DISCORD_TOKEN = process.env.DISCORD_APP_TOKEN;
-const DISCORD_CLIENT_ID = process.env.DISCORD_CLIENT_ID;
+// const DISCORD_TOKEN = process.env.DISCORD_APP_TOKEN;
+// const DISCORD_CLIENT_ID = process.env.DISCORD_CLIENT_ID;
 
-const commands: RESTPostAPIApplicationCommandsJSONBody[] = [
-  {
-    name: "carlinhos",
-    description: "fala a verdade.",
-  },
-];
+// const commands: RESTPostAPIApplicationCommandsJSONBody[] = [
+// 	{
+// 		name: "carlinhos",
+// 		description: "fala a verdade.",
+// 	},
+// ];
 
-async function main() {
-  if (!DISCORD_TOKEN || !DISCORD_CLIENT_ID) {
-    throw new Error(chalk.red("Missing token or client ID"));
-  }
+// async function main() {
+// 	if (!DISCORD_TOKEN || !DISCORD_CLIENT_ID) {
+// 		throw new Error(chalk.red("Missing token or client ID"));
+// 	}
 
-  try {
-    await registerCommands(DISCORD_TOKEN, DISCORD_CLIENT_ID, commands);
+// 	try {
+// 		await registerCommands(DISCORD_TOKEN, DISCORD_CLIENT_ID, commands);
 
-    const client = new Client({
-      intents: [GatewayIntentBits.Guilds],
-    });
+// 		const client = new Client({
+// 			intents: [GatewayIntentBits.Guilds],
+// 		});
 
-    client.once("ready", async (c) => {
-      console.log(chalk.green(`${c.user.username} is online`));
-    });
+// 		client.once("ready", async (c) => {
+// 			console.log(chalk.green(`${c.user.username} is online`));
+// 		});
 
-    client.on("interactionCreate", async (interaction) => {
-      if (!interaction.isChatInputCommand()) return;
+// 		client.on("interactionCreate", async (interaction) => {
+// 			if (!interaction.isChatInputCommand()) return;
 
-      if (interaction.commandName === "carlinhos") {
-        await interaction.reply("é poggers");
-      }
-    });
+// 			if (interaction.commandName === "carlinhos") {
+// 				await interaction.reply("é poggers");
+// 			}
+// 		});
 
-    console.log(chalk.gray("Logging in..."));
-    await client.login(DISCORD_TOKEN);
-  } catch (error) {
-    throw new Error(chalk.red(`Error iniatializing the bot: ${error}`));
-  }
+// 		console.log(chalk.gray("Logging in..."));
+// 		await client.login(DISCORD_TOKEN);
+// 	} catch (error) {
+// 		throw new Error(chalk.red(`Error iniatializing the bot: ${error}`));
+// 	}
+// }
+// main();
+
+export class Main {
+	private static DiscordClient = new Client({
+		intents: [GatewayIntentBits.Guilds],
+	});
+
+	public static Initialize() {
+		Logger.Info("Inicializando a bomba...");
+
+		Main.DiscordClient.login(process.env.DISCORD_APP_TOKEN);
+
+		Logger.Success("ligo");
+	}
 }
-main();
+
+Main.Initialize();
