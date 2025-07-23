2ddf0459660d27a440e79bee29829490f4de5c52
Matheus <matheusp.sampaio@gmail.com>
2025-07-23 20:57:23 -0300

    Adds an initial structure for repository layer

---
+++ src/Repositories/LobbiesRepository.ts
@@ -0,0 +1,20 @@
+import { prisma } from "../lib/prisma";
+
+export class LobbiesRepository {
+
+    public static async Create(user: string, date_timestamp: number) {
+
+    }
+
+    public static async Get(user: string) {
+
+    }
+
+    public static async Update(user: string, date_timestamp: number) {
+
+    }
+
+    public static async Delete(user: string) {
+        
+    }
+}
