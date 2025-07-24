b7f0df8e188d4b241a9ee28e0758496b16d47a4a
Vitor Gabriel <37278974+zNyash@users.noreply.github.com>
2025-07-24 12:20:07 -0300

    Format codebase & remove unused files

--- src/Bancho/BanchoClient.ts
+++ src/Bancho/BanchoClient.ts
@@ -2,18 +2,18 @@ import irc from "bancho.js";
 import { Logger } from "../utils/Logger";
 
 export class BanchoManager {
-	public static readonly BanchoClient = new irc.BanchoClient({
-		username: process.env.BANCHO_IRC_USERNAME!,
-		password: process.env.BANCHO_IRC_PASSWORD!,
-	});
+    public static readonly BanchoClient = new irc.BanchoClient({
+        username: process.env.BANCHO_IRC_USERNAME!,
+        password: process.env.BANCHO_IRC_PASSWORD!,
+    });
 
-	public static async Connect() {
-		Logger.Info("Connecting to Bancho server");
+    public static async Connect() {
+        Logger.Info("Connecting to Bancho server");
 
-		await BanchoManager.BanchoClient.connect();
+        await BanchoManager.BanchoClient.connect();
 
-		Logger.Success("Connected to Bancho and listening to messages");
-	}
+        Logger.Success("Connected to Bancho and listening to messages");
+    }
 
-	private static async HandleMessage() {}
+    private static async HandleMessage() {}
 }
--- src/index.ts
+++ src/index.ts
@@ -7,25 +7,25 @@ import { BanchoManager } from "./Bancho/BanchoClient";
 config();
 
 export class Main {
-	public static readonly DiscordClient = new Client({
-		intents: [GatewayIntentBits.Guilds],
-	});
+    public static readonly DiscordClient = new Client({
+        intents: [GatewayIntentBits.Guilds],
+    });
 
-	public static async Initialize() {
-		Logger.Info("Inicializando a bomba...");
+    public static async Initialize() {
+        Logger.Info("Inicializando a bomba...");
 
-		await Main.DiscordClient.login(process.env.DISCORD_APP_TOKEN);
-		await CommandsManager.InitializeCommands();
-		await BanchoManager.Connect();
+        await Main.DiscordClient.login(process.env.DISCORD_APP_TOKEN);
+        await CommandsManager.InitializeCommands();
+        await BanchoManager.Connect();
 
-		Main.DiscordClient.on("interactionCreate", (interaction) => {
-			if (interaction.isChatInputCommand()) {
-				CommandsManager.HandleCommandInteraction(interaction);
-			}
-		});
+        Main.DiscordClient.on("interactionCreate", (interaction) => {
+            if (interaction.isChatInputCommand()) {
+                CommandsManager.HandleCommandInteraction(interaction);
+            }
+        });
 
-		Logger.Success("ligo");
-	}
+        Logger.Success("ligo");
+    }
 }
 
 Main.Initialize();
