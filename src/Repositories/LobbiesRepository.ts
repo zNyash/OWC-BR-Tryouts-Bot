9f6a5c5bf3f1b418a6d21b4cecd86a0cd62d8a5f
Matheus <matheusp.sampaio@gmail.com>
2025-07-24 10:27:58 -0300

    Implementation of Create, Get and Update on LobbiesRepository

--- src/Repositories/LobbiesRepository.ts
+++ src/Repositories/LobbiesRepository.ts
@@ -1,20 +1,63 @@
 import { prisma } from "../lib/prisma";
+import { Logger } from "../utils/Logger";
 
 export class LobbiesRepository {
 
-    public static async Create(user: string, date_timestamp: number) {
+    public static async Create(user: string, date_timestamp: bigint) {
+        Logger.Info(`Inserting a new lobby schedule for user ${user}`);
 
+        try{
+            const lobby = await prisma.lobbies.create({
+                data: {
+                    player: user,
+                    date: date_timestamp
+                }
+            });
+
+            Logger.Success("New lobby inserted")
+
+            return lobby;
+        }
+        catch(error) {
+            Logger.Error(error);
+            
+            return null;
+        }
     }
 
     public static async Get(user: string) {
+        Logger.Info(`Fetching a lobby for user ${user}`);
 
+        const lobby = await prisma.lobbies.findUnique({
+            where: {
+                player: user
+            }
+        });
+
+        return lobby;
     }
 
-    public static async Update(user: string, date_timestamp: number) {
+    public static async Update(user: string, date_timestamp: bigint) {
+        Logger.Info(`Updating lobby schedule for user ${user}`);
 
-    }
+        try{
+            const lobby = await prisma.lobbies.update({
+                where: {
+                    player: user
+                },
+                data: {
+                    date: date_timestamp
+                }
+            });
 
-    public static async Delete(user: string) {
-        
+            Logger.Success("Lobby updated")
+
+            return lobby;
+        }
+        catch(error) {
+            Logger.Error(error);
+            
+            return null;
+        } 
     }
 }
